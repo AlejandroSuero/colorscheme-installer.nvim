0d55c4405e6cd221be6f284cda4d24e1ed7028c5
AlejandroSuero <asuerome@gmail.com>
2024-05-06 16:30:10 +0200

    fix(minimal.lua): formatted

--- contrib/minimal.lua
+++ contrib/minimal.lua
@@ -3,26 +3,25 @@ vim.cmd([[set packpath=/tmp/nvim/lazy/]])
 
 local lazypath = "/tmp/nvim/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{
+  {
     "AlejandroSuero/colorscheme-installer.nvim",
-		config = function()
+    config = function()
       require("colorscheme-installer").setup()
-		end,
+    end,
     lazy = false,
-		enabled = true,
-	},
+    enabled = true,
+  },
 })
-
